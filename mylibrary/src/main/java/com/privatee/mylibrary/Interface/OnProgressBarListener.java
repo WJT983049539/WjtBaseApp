8b7fc80ba522499f75dda50c2d7f5d2d8cfa2cd0
wjt <983049539@qq.com>
2017-11-23 17:51:47 +0800

    interface

---
+++ mylibrary/src/main/java/com/privatee/mylibrary/Interface/OnProgressBarListener.java
@@ -0,0 +1,9 @@
+package com.privatee.mylibrary.Interface;
+
+/**
+ * Created by wjt on 15-4-23.
+ */
+public interface OnProgressBarListener {
+
+    void onProgressChange(int current, int max);
+}
---
+++ mylibrary/src/main/java/com/privatee/mylibrary/Interface/examineInterface.java
@@ -0,0 +1,9 @@
+package com.privatee.mylibrary.Interface;
+
+/**
+ * 类的作用：
+ * Created by WJT on  2017/11/23 14:48.
+ */
+
+public interface examineInterface {
+}
